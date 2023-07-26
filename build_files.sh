d0dddb7971caf5885a28cd7f01766d9396d799dc
Mikhail Gostev <he.is.really.experienced@gmail.com>
2023-07-27 00:05:30 +0300

    upd

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,5 @@
 pip install -r requirements.txt
 
 # make migrations
-python3.0 manage.py migrate 
-python3.9 manage.py collectstatic
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic --clear
