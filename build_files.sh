b5bb024b923ebb285f1206c03c3905df6fdfcac2
Mikhail Gostev <he.is.really.experienced@gmail.com>
2023-07-26 23:34:08 +0300

    trying

--- build_files.sh
+++ build_files.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# Create a virtual environment
-echo "Creating a virtual environment..."
-python3.9 -m venv venv
-source venv/bin/activate
+pip install -r requirements.txt
 
-# Install the latest version of pip
-echo "Installing the latest version of pip..."
-python -m pip install --upgrade pip
-
-# Build the project
-echo "Building the project..."
-python -m pip install -r requirements.txt
-
-# Make migrations
-echo "Making migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-
-# Collect static files
-echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+# make migrations
+python manage.py migrate 
+python manage.py collectstatic
